29b4d9982fb4b951efec44ee567a92d9258df4cc
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-08-21 11:59:23 +0300

    каруселя

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,14 @@ $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 		items: 3
 	});
+	$(".owl-carousel2").owlCarousel({
+		items: 3
+	});
 
 
-	$('.ic_view').on("click", function(){
-		
+	$('.eye').on("click", function(){
+		$(this).parents('div.col-md-3').css('width', '100%');
+		$(this).parents('.product-img-description').next().css({'display':'block','float':'left'});
+		$(this).css({'float':'left'});
 	});
 });
