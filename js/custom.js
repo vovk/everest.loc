0d7174f3933c8530f8a24d2eaf0666e4cc55681c
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-08-21 12:51:47 +0300

    стрелочки пока что не работают

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,17 @@ $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 		items: 3
 	});
-	$(".owl-carousel2").owlCarousel({
-		items: 3
-	});
-
+	$('#catalogSlider .owl-carousel').owlCarousel({
+		items: 3,
+        nav: 1,
+        navText: ["prev", "next"],
+        navContainerClass: "owl-nav",
+        navClass: ["owl-prev", "owl-next"],
+        slideBy: 1,
+    });
 
 	$('.eye').on("click", function(){
 		$(this).parents('div.col-md-3').css('width', '100%');
 		$(this).parents('.product-img-description').next().css({'display':'block','float':'left'});
-		$(this).css({'float':'left'});
 	});
 });
