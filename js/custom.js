bd3affc13b2ab35c5345839cdd905945eeb18f96
Jeki4 <gerasim_93@i.ua>
2017-08-21 14:01:40 +0300

    Fixed owl-carousel-nav

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
 	// owl-carousel
-	$(".owl-carousel").owlCarousel({
+	$(".categories-content .owl-carousel").owlCarousel({
 		items: 3
 	});
 	$('#catalogSlider .owl-carousel').owlCarousel({
 		items: 3,
-        nav: 1,
-        navText: ["prev", "next"],
-        navContainerClass: "owl-nav",
-        navClass: ["owl-prev", "owl-next"],
+       // nav : true,
+       // navText: ['<button class="owl-prev">Prev</button>', '<button class="owl-next">Next</button>'],
+       // navContainerClass: "owl-nav",
+       // navClass: ["owl-prev", "owl-next"],
         slideBy: 1,
     });
+
+
+document.getElementById('next-slide').addEventListener('click', function(){
+   $('#catalogSlider .owl-carousel').trigger('next.owl.carousel');
+});
+
+document.getElementById('prev-slide').addEventListener('click', function(){
+   $('#catalogSlider .owl-carousel').trigger('prev.owl.carousel');
+});
+
 
 	$('.eye').on("click", function(){
 		$(this).parents('div.col-md-3').css('width', '100%');
