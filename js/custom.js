3d1cc9544145414d62d351abfc0445082418a17d
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-08-22 11:03:35 +0300

    product_bedroom 7 слайд

--- js/custom.js
+++ js/custom.js
@@ -30,11 +30,27 @@ $(document).ready(function(){
             }
         }
     });
-    document.getElementById('next-slide').addEventListener('click', function(){
-        $('#popularProducts').trigger('next.owl.carousel');
-    });
 
-    document.getElementById('prev-slide').addEventListener('click', function(){
-        $('#popularProducts').trigger('prev.owl.carousel');
+    var element1 = document.getElementById('next-slide');
+    var element2 = document.getElementById('prev-slide');
+    if (element1){
+	    document.getElementById('next-slide').addEventListener('click', function(){
+	        $('#popularProducts').trigger('next.owl.carousel');
+	    });
+	}
+	if (element2){
+	    document.getElementById('prev-slide').addEventListener('click', function(){
+	        $('#popularProducts').trigger('prev.owl.carousel');
+	    });
+	}
+
+    // tabs
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
     });
 });
