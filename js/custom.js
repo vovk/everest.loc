1a62357da54a12b7b30f7944a00e6ae59d68d2d4
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-08-31 19:26:26 +0300

    красивый order.html

--- js/custom.js
+++ js/custom.js
@@ -57,34 +57,66 @@ $(document).ready(function(){
     //cart quantity
     jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="image/ic_arrow_drop_up.png"></div><div class="quantity-button quantity-down"><img src="image/arrow-down.png"></div></div>').insertAfter('.quantity input');
     jQuery('.quantity').each(function() {
-      var spinner = jQuery(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
 
-      btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
 
-      btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
 
     });
+
+
+    //процесс заказа
+    $('.order-container1 .next1').on("click", function(){
+    	$(this).closest('.order-container1').toggle(700);
+    	$('.order-container1').next('.order-container2').toggle(400);
+    	$('.ord.this').animate(
+    		{
+    			backgroundColor: '#000',
+    			color: '#fff'
+    		}, 
+    		700,
+    		'easeInSine');
+    	$(this).closest('.order-container1').siblings('.wrapper:first').find('.image-processing>img').css('height', '2px');
+    	$(this).closest('.order-container1').siblings('.wrapper.wrapper2').find('.ord').addClass('this');
+    });
+
+    $('.order-container2 .next2').on("click", function(){
+		if(($('.order-container2').is(':visible'))){
+			$(this).closest('.order-container2').toggle(700);
+			$('.order-container2').next('.order-container3').toggle(400);
+			$('.ord.this').animate(
+    		{
+    			backgroundColor: '#000',
+    			color: '#fff'
+    		}, 
+    		700,
+    		'easeInSine');
+    		$(this).closest('.order-container2').siblings('.wrapper.wrapper2').find('.image-processing>img').css('height', '2px');
+			$(this).closest('.order-container2').siblings('.wrapper.wrapper3').find('.ord').addClass('this');
+		}
+	});
 });
