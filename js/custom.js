cccd0e484ca1e233e095279e13bcbbf58db93567
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-09-19 17:01:48 +0300

    media.css custom.js <======== tabs

--- js/custom.js
+++ js/custom.js
@@ -45,14 +45,50 @@ $(document).ready(function(){
 	}
 
     // tabs
-    $(".tabs-menu a").click(function(event) {
-        event.preventDefault();
-        $(this).parent().addClass("current");
-        $(this).parent().siblings().removeClass("current");
-        var tab = $(this).attr("href");
-        $(".tab-content").not(tab).css("display", "none");
-        $(tab).fadeIn();
+    // $(".tabs-menu a").click(function(event) {
+    //     event.preventDefault();
+    //     $(this).parent().addClass("current");
+    //     $(this).parent().siblings().removeClass("current");
+    //     var tab = $(this).attr("href");
+    //     $(".tab-content").not(tab).css("display", "none");
+    //     $(tab).fadeIn();
+    // });
+    $(".tab-content").hide();
+    $(".tab-content:first").show();
+
+    /* if in tab mode */
+    $("ul.tabs-menu li").click(function() {
+		
+      $(".tab-content").hide();
+      var activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn();		
+		
+      $("ul.tabs-menu li").removeClass("current");
+      $(this).addClass("current");
+
+	  $(".tab_drawer_heading").removeClass("d_active");
+	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+	  
     });
+	/* if in drawer mode */
+	$(".tab_drawer_heading").click(function() {
+      
+      $(".tab-content").hide();
+      var d_activeTab = $(this).attr("rel"); 
+      $("#"+d_activeTab).fadeIn();
+	  
+	  $(".tab_drawer_heading").removeClass("d_active");
+      $(this).addClass("d_active");
+	  
+	  $("ul.tabs-menu li").removeClass("active");
+	  $("ul.tabs-menu li[rel^='"+d_activeTab+"']").addClass("active");
+    });
+	
+	
+	/* Extra class "tab_last" 
+	   to add border to right side
+	   of last tab */
+	$('ul.tabs li').last().addClass("tab_last");
 
     //cart quantity
     jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="image/ic_arrow_drop_up.png"></div><div class="quantity-button quantity-down"><img src="image/arrow-down.png"></div></div>').insertAfter('.quantity input');
