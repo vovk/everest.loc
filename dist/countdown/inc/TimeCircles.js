6d93cad7b537376acf3d40e11dabd2b56905649f
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-09-22 14:19:28 +0300

    исправления

--- dist/countdown/inc/TimeCircles.js
+++ dist/countdown/inc/TimeCircles.js
@@ -13,6 +13,7 @@
  * Each function called upon the public class will be forwarded to each instance
  * of the private classes within the relevant element collection
  **/
+
 (function($) {
 
     var useWindow = window;
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	// owl-carousel
 	$(".imageCategory .owl-carousel").owlCarousel({
         responsive: {
